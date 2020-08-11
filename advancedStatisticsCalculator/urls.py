6fd6612c3a3bc7b8599110c197a3581f08636df3
JoseRuizCasado <53489806+JoseRuizCasado@users.noreply.github.com>
2020-08-11 11:40:04 +0200

    MakeInform endpoint added.

--- advancedStatisticsCalculator/urls.py
+++ advancedStatisticsCalculator/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('team-stats/<int:team_id>', GetTeamStats.as_view(), name='team-stats'),
-    path('team-players-stats/<int:team_id>', GetTeamPlayersStats.as_view(), name='team-players-stats')
+    path('team-players-stats/<int:team_id>', GetTeamPlayersStats.as_view(), name='team-players-stats'),
+    path('make-inform/<int:team_id>/vs/<int:opponent_team_id>', MakeInform.as_view(), name='make-inform')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- advancedStatisticsCalculator/views.py
+++ advancedStatisticsCalculator/views.py
@@ -3,6 +3,57 @@ import requests
 from .StatsCalculator import *
 
 
+def calculate_advance_team_stats(team_json):
+    # Calculate advanced stats with stats from de database
+    team_json['TmOffRtg'] = team_off_rtg(team_json['scored_points'], team_json['field_goals_attempts'],
+                                         team_json['field_goals_made'], team_json['turnovers'],
+                                         team_json['free_throws_attempts'],
+                                         team_json['offensive_rebounds'], team_json['conceded_defensive_rebounds'])
+    team_json['TmFloor%'] = team_floor_per(team_json['field_goals_attempts'],
+                                           team_json['field_goals_made'], team_json['turnovers'],
+                                           team_json['free_throws_attempts'], team_json['free_throws_made'],
+                                           team_json['offensive_rebounds'],
+                                           team_json['conceded_defensive_rebounds'])
+    team_json['DefRtg'] = team_def_rtg(team_json['conceded_points'], team_json['field_goals_attempts'],
+                                       team_json['field_goals_made'], team_json['turnovers'],
+                                       team_json['free_throws_attempts'],
+                                       team_json['offensive_rebounds'], team_json['conceded_defensive_rebounds'])
+    team_json['NetRtg'] = team_json['TmOffRtg'] - team_json['DefRtg']
+    team_json['Pace'] = team_pace((team_json['played_minutes'] / 60), team_json['field_goals_attempts'],
+                                  team_json['field_goals_made'], team_json['turnovers'],
+                                  team_json['free_throws_attempts'], team_json['offensive_rebounds'],
+                                  team_json['conceded_defensive_rebounds'],
+                                  team_json['conceded_field_goals_attempts'],
+                                  team_json['conceded_field_goals_made'], team_json['conceded_turnovers'],
+                                  team_json['conceded_free_throws_attempts'],
+                                  team_json['conceded_offensive_rebounds'],
+                                  team_json['defensive_rebounds'])
+    team_json['TS%'] = true_shooting_per(team_json['scored_points'], team_json['field_goals_attempts'],
+                                         team_json['free_throws_attempts'])
+    two_field_goals = team_json['field_goals_made'] - team_json['three_points_field_goals_made']
+    team_json['eFG%'] = effective_field_goals_per(two_field_goals, team_json['three_points_field_goals_made'],
+                                                  team_json['field_goals_attempts'])
+    team_json['FTARate'] = free_throws_att_rate(team_json['free_throws_attempts'],
+                                                team_json['field_goals_attempts'])
+    team_json['3FGARate'] = three_field_goals_att_rate(team_json['three_points_field_goals_attempts'],
+                                                       team_json['field_goals_attempts'])
+    team_json['TmOR%'] = team_off_rebound_per(team_json['offensive_rebounds'],
+                                              team_json['conceded_defensive_rebounds'])
+    team_json['TmDR%'] = team_def_rebound_per(team_json['defensive_rebounds'],
+                                              team_json['conceded_offensive_rebounds'])
+    team_json['BLK%'] = team_blocks_per(team_json['blocks'], team_json['conceded_field_goals_attempts'],
+                                        team_json['conceded_three_points_field_goals_attempts'])
+    team_json['TOV%'] = turnovers_per(team_json['turnovers'], team_json['field_goals_attempts'],
+                                      team_json['free_throws_attempts'])
+    team_json['AST%'] = team_assists_per(team_json['assists'], team_json['field_goals_attempts'])
+    team_json['STL%'] = team_steals_per(team_json['steals'], team_json['conceded_field_goals_attempts'],
+                                        team_json['conceded_field_goals_made'], team_json['conceded_turnovers'],
+                                        team_json['conceded_free_throws_attempts'],
+                                        team_json['conceded_offensive_rebounds'],
+                                        team_json['defensive_rebounds'])
+    return team_json
+
+
 class GetTeamStats(views.APIView):
 
     @staticmethod
@@ -16,53 +67,7 @@ class GetTeamStats(views.APIView):
         # Get Team stats stored in database
         print(f'TEAM REQUEST: {team_id}')
         team_json = requests.get(url=f'http://127.0.0.1:8000/dbmanager/team/{team_id}').json()['team']
-        # Calculate advanced stats with stats from de database
-        team_json['TmOffRtg'] = team_off_rtg(team_json['scored_points'], team_json['field_goals_attempts'],
-                                             team_json['field_goals_made'], team_json['turnovers'],
-                                             team_json['free_throws_attempts'],
-                                             team_json['offensive_rebounds'], team_json['conceded_defensive_rebounds'])
-        team_json['TmFloor%'] = team_floor_per(team_json['field_goals_attempts'],
-                                               team_json['field_goals_made'], team_json['turnovers'],
-                                               team_json['free_throws_attempts'], team_json['free_throws_made'],
-                                               team_json['offensive_rebounds'],
-                                               team_json['conceded_defensive_rebounds'])
-        team_json['DefRtg'] = team_def_rtg(team_json['conceded_points'], team_json['field_goals_attempts'],
-                                           team_json['field_goals_made'], team_json['turnovers'],
-                                           team_json['free_throws_attempts'],
-                                           team_json['offensive_rebounds'], team_json['conceded_defensive_rebounds'])
-        team_json['NetRtg'] = team_json['TmOffRtg'] - team_json['DefRtg']
-        team_json['Pace'] = team_pace((team_json['played_minutes'] / 60), team_json['field_goals_attempts'],
-                                      team_json['field_goals_made'], team_json['turnovers'],
-                                      team_json['free_throws_attempts'], team_json['offensive_rebounds'],
-                                      team_json['conceded_defensive_rebounds'],
-                                      team_json['conceded_field_goals_attempts'],
-                                      team_json['conceded_field_goals_made'], team_json['conceded_turnovers'],
-                                      team_json['conceded_free_throws_attempts'],
-                                      team_json['conceded_offensive_rebounds'],
-                                      team_json['defensive_rebounds'])
-        team_json['TS%'] = true_shooting_per(team_json['scored_points'], team_json['field_goals_attempts'],
-                                             team_json['free_throws_attempts'])
-        two_field_goals = team_json['field_goals_made'] - team_json['three_points_field_goals_made']
-        team_json['eFG%'] = effective_field_goals_per(two_field_goals, team_json['three_points_field_goals_made'],
-                                                      team_json['field_goals_attempts'])
-        team_json['FTARate'] = free_throws_att_rate(team_json['free_throws_attempts'],
-                                                    team_json['field_goals_attempts'])
-        team_json['3FGARate'] = three_field_goals_att_rate(team_json['three_points_field_goals_attempts'],
-                                                           team_json['field_goals_attempts'])
-        team_json['TmOR%'] = team_off_rebound_per(team_json['offensive_rebounds'],
-                                                  team_json['conceded_defensive_rebounds'])
-        team_json['TmDR%'] = team_def_rebound_per(team_json['defensive_rebounds'],
-                                                  team_json['conceded_offensive_rebounds'])
-        team_json['BLK%'] = team_blocks_per(team_json['blocks'], team_json['conceded_field_goals_attempts'],
-                                            team_json['conceded_three_points_field_goals_attempts'])
-        team_json['TOV%'] = turnovers_per(team_json['turnovers'], team_json['field_goals_attempts'],
-                                          team_json['free_throws_attempts'])
-        team_json['AST%'] = team_assists_per(team_json['assists'], team_json['field_goals_attempts'])
-        team_json['STL%'] = team_steals_per(team_json['steals'], team_json['conceded_field_goals_attempts'],
-                                            team_json['conceded_field_goals_made'], team_json['conceded_turnovers'],
-                                            team_json['conceded_free_throws_attempts'],
-                                            team_json['conceded_offensive_rebounds'],
-                                            team_json['defensive_rebounds'])
+        team_json = calculate_advance_team_stats(team_json)
 
         return response.Response(data={'team': team_json}, status=status.HTTP_200_OK)
 
@@ -80,53 +85,7 @@ class GetTeamPlayersStats(views.APIView):
         # Get Team stats stored in database
         team_json = requests.get(url=f'http://127.0.0.1:8000/dbmanager/team/{team_id}').json()['team']
         # Calculate advanced stats with stats from de database
-        team_json['TmOffRtg'] = team_off_rtg(team_json['scored_points'], team_json['field_goals_attempts'],
-                                             team_json['field_goals_made'], team_json['turnovers'],
-                                             team_json['free_throws_attempts'],
-                                             team_json['offensive_rebounds'], team_json['conceded_defensive_rebounds'])
-        team_json['TmFloor%'] = team_floor_per(team_json['field_goals_attempts'],
-                                               team_json['field_goals_made'], team_json['turnovers'],
-                                               team_json['free_throws_attempts'], team_json['free_throws_made'],
-                                               team_json['offensive_rebounds'],
-                                               team_json['conceded_defensive_rebounds'])
-        team_json['DefRtg'] = team_def_rtg(team_json['conceded_points'], team_json['field_goals_attempts'],
-                                           team_json['field_goals_made'], team_json['turnovers'],
-                                           team_json['free_throws_attempts'],
-                                           team_json['offensive_rebounds'], team_json['conceded_defensive_rebounds'])
-        team_json['NetRtg'] = team_json['TmOffRtg'] - team_json['DefRtg']
-        team_json['Pace'] = team_pace((team_json['played_minutes'] / 60), team_json['field_goals_attempts'],
-                                      team_json['field_goals_made'], team_json['turnovers'],
-                                      team_json['free_throws_attempts'], team_json['offensive_rebounds'],
-                                      team_json['conceded_defensive_rebounds'],
-                                      team_json['conceded_field_goals_attempts'],
-                                      team_json['conceded_field_goals_made'], team_json['conceded_turnovers'],
-                                      team_json['conceded_free_throws_attempts'],
-                                      team_json['conceded_offensive_rebounds'],
-                                      team_json['defensive_rebounds']
-                                      )
-        team_json['TS%'] = true_shooting_per(team_json['scored_points'], team_json['field_goals_attempts'],
-                                             team_json['free_throws_attempts'])
-        two_field_goals = team_json['field_goals_made'] - team_json['three_points_field_goals_made']
-        team_json['eFG%'] = effective_field_goals_per(two_field_goals, team_json['three_points_field_goals_made'],
-                                                      team_json['field_goals_attempts'])
-        team_json['FTARate'] = free_throws_att_rate(team_json['free_throws_attempts'],
-                                                    team_json['field_goals_attempts'])
-        team_json['3FGARate'] = three_field_goals_att_rate(team_json['three_points_field_goals_attempts'],
-                                                           team_json['field_goals_attempts'])
-        team_json['TmOR%'] = team_off_rebound_per(team_json['offensive_rebounds'],
-                                                  team_json['conceded_defensive_rebounds'])
-        team_json['TmDR%'] = team_def_rebound_per(team_json['defensive_rebounds'],
-                                                  team_json['conceded_offensive_rebounds'])
-        team_json['BLK%'] = team_blocks_per(team_json['blocks'], team_json['conceded_field_goals_attempts'],
-                                            team_json['conceded_three_points_field_goals_attempts'])
-        team_json['TOV%'] = turnovers_per(team_json['turnovers'], team_json['field_goals_attempts'],
-                                          team_json['free_throws_attempts'])
-        team_json['AST%'] = team_assists_per(team_json['assists'], team_json['field_goals_attempts'])
-        team_json['STL%'] = team_steals_per(team_json['steals'], team_json['conceded_field_goals_attempts'],
-                                            team_json['conceded_field_goals_made'], team_json['conceded_turnovers'],
-                                            team_json['conceded_free_throws_attempts'],
-                                            team_json['conceded_offensive_rebounds'],
-                                            team_json['defensive_rebounds'])
+        team_json = calculate_advance_team_stats(team_json)
 
         # Load team players
         players_json = requests.get(url=f'http://127.0.0.1:8000/dbmanager/players-from-team/{team_id}').json()
@@ -215,3 +174,52 @@ class GetTeamPlayersStats(views.APIView):
 
         team_json['players'] = players_list
         return response.Response(data={'team': team_json}, status=status.HTTP_200_OK)
+
+
+class MakeInform(views.APIView):
+
+    @staticmethod
+    def get(request, team_id, opponent_team_id):
+        team_json = requests.get(url=f'http://127.0.0.1:8000/dbmanager/team/{team_id}').json()['team']
+        calculate_advance_team_stats(team_json)
+        opponent_team_json = requests.get(url=f'http://127.0.0.1:8000/dbmanager/team/{opponent_team_id}').json()['team']
+        calculate_advance_team_stats(opponent_team_json)
+        data = {
+            'Local_TmOffRtg': team_json['TmOffRtg'],
+            'Local_TmFloor%': team_json['TmFloor%'],
+            'Local_TmDefRtg': team_json['DefRtg'],
+            'Local_Pace': team_json['Pace'],
+            'Local_TS%': team_json['TS%'],
+            'Local_eFG%': team_json['eFG%'],
+            'Local_FTARate': team_json['FTARate'],
+            'Local_3FGARate': team_json['3FGARate'],
+            'Local_TmOR%': team_json['TmOR%'],
+            'Local_TmDR%': team_json['TmDR%'],
+            'Local_BLK%': team_json['BLK%'],
+            'Local_TOV%': team_json['TOV%'],
+            'Local_STL%': team_json['STL%'],
+            'Visitor_TmOffRtg': opponent_team_json['TmOffRtg'],
+            'Visitor_TmFloor%': opponent_team_json['TmFloor%'],
+            'Visitor_TmDefRtg': opponent_team_json['DefRtg'],
+            'Visitor_Pace': opponent_team_json['Pace'],
+            'Visitor_TS%': opponent_team_json['TS%'],
+            'Visitor_eFG%': opponent_team_json['eFG%'],
+            'Visitor_FTARate': opponent_team_json['FTARate'],
+            'Visitor_3FGARate': opponent_team_json['3FGARate'],
+            'Visitor_TmOR%': opponent_team_json['TmOR%'],
+            'Visitor_TmDR%': opponent_team_json['TmDR%'],
+            'Visitor_BLK%': opponent_team_json['BLK%'],
+            'Visitor_TOV%': opponent_team_json['TOV%'],
+            'Visitor_STL%': opponent_team_json['STL%']
+        }
+        local_prediction = requests.get(url='http://127.0.0.1:8000/modelPredictor/',
+                                        json={'data': data}).json()['prediction']
+        if local_prediction == 'Win':
+            opponent_prediction = 'Lose'
+        else:
+            opponent_prediction = 'Win'
+
+        data['Local_Prediction'] = local_prediction
+        data['Visitor_Prediction'] = opponent_prediction
+
+        return response.Response(data=data, status=status.HTTP_200_OK)
